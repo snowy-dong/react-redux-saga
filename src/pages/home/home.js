5d1a1496aa78a2e80aa0a5054799245d33a909e3
snowy-dong <99186652@qq.com>
2018-07-20 16:48:48 +0800

    '更新'

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -11,6 +11,7 @@ export default class Home extends React.Component{
   render(){
     return (
       <div className="home">
+          <StarCarousel/>
           <div  className="Portals" >
             <Row gutter={16}>
               <Col className="gutter-row" span={6}>
