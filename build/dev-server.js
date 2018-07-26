6557f1eff343fdd51b167d6b3a76f49cd54f68a5
snowy-dong <99186652@qq.com>
2018-07-26 11:47:14 +0800

    更新

--- build/dev-server.js
+++ build/dev-server.js
@@ -34,7 +34,6 @@ var hotMiddleware = require('webpack-hot-middleware')(compiler, {
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
     hotMiddleware.publish({ action: 'reload' });
-    cb();
   });
 });
 
